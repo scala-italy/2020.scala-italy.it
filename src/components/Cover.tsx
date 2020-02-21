bcaeb563c9720a0c0efcb9a4ca1f4891831d09be
Gabriele Petronella <gabriele@buildo.io>
2020-02-21 11:00:22 +0100

    Fix static

--- src/components/Cover.tsx
+++ src/components/Cover.tsx
@@ -3,7 +3,7 @@ import styles from "./cover.module.css"
 
 export const Cover = () => (
   <div className={styles.cover}>
-    <img className={styles.logo} src={"./logo.png"} alt="logo" />
+    <img className={styles.logo} src={"/logo.png"} alt="logo" />
     <h1>The Italian conference on Scala</h1>
     <h3>Milano, September 2020</h3>
   </div>
