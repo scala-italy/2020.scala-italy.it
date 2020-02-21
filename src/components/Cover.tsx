7a1f2256f8a1d8599a7f29566de6750a6b4601a0
Gabriele Petronella <gabriele@buildo.io>
2020-02-21 16:55:06 +0100

    Import assets

--- src/components/Cover.tsx
+++ src/components/Cover.tsx
@@ -1,9 +1,11 @@
 import React from "react"
 import styles from "./cover.module.css"
 
+import logo from "./logo.png"
+
 export const Cover = () => (
   <div className={styles.cover}>
-    <img className={styles.logo} src={"/logo.png"} alt="logo" />
+    <img className={styles.logo} src={logo} alt="Scala Italy Logo" />
     <h1>The Italian conference on Scala</h1>
     <h3>Milano, September 2020</h3>
   </div>
