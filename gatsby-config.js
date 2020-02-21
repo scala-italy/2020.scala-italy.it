b753a784eaf02bc86f7efa11dbc06e129e4ff900
Marco F <marco.firrincieli@unicredit.eu>
2020-02-21 11:17:16 +0100

    gatsby-plugin-google-analytics added

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,7 +3,27 @@
  *
  * See: https://www.gatsbyjs.org/docs/gatsby-config/
  */
-
 module.exports = {
-  plugins: ["gatsby-plugin-typescript"],
+  plugins: ["gatsby-plugin-typescript",
+    {
+      resolve: "gatsby-plugin-google-analytics",
+      options: {
+        // The property ID; the tracking code won't be generated without it
+        trackingId: "UA-58476507-1",
+        // Defines where to place the tracking script - `true` in the head and `false` in the body
+        head: false,
+        // Setting this parameter is optional
+        anonymize: true,
+        // Setting this parameter is also optional
+        respectDNT: true,
+        // Avoids sending pageview hits from custom paths
+        // exclude: ["/preview/**", "/do-not-track/me/too/"],
+        // Delays sending pageview hits on route update (in milliseconds)
+        pageTransitionDelay: 0,
+        sampleRate: 5,
+        siteSpeedSampleRate: 10,
+        cookieDomain: "example.com",
+      },
+    }
+  ],
 }
